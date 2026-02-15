c68d004aaa3f153f7d02f1aeffe6b89d42bac819
maho1227 <mahomadoka1227@gmail.com>
2026-02-15 13:55:54 +0900

    user_idの型を変更

---
+++ database/migrations/2026_02_15_045436_fix_user_id_type_in_archives.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('archives', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+
+        Schema::table('archives', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('archives', function (Blueprint $table) {
+            //
+        });
+    }
+};
